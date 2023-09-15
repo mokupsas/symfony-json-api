e9f7f59c16da5f665f3503a8c718d3948cb343eb
mokupsas <mockus.ri@gmail.com>
2023-09-15 23:51:50 +0300

    BaseController: create base controller

---
+++ src/Core/BaseController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Core;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class BaseController extends AbstractController
+{
+    public function ResponseOK(array $data = [])
+    {
+        return $this->ResponseJSON(true, $data);
+    }
+
+    public function ResponseFail(string $msg)
+    {
+        return $this->ResponseJSON(false, ['message' => $msg]);
+    }
+
+    public function ResponseError(string $error_msg)
+    {
+        return $this->ResponseJSON(false, ['error' => $error_msg]);
+    }
+
+    private function ResponseJSON(bool $success, $data)
+    {
+        return new JsonResponse([
+            'success'   => $success,
+            'data'      => $data
+        ]);
+    }
+}
